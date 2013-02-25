10922a9794657ab611a13b5faaaada470a3e46ce
Daniel Hopkins <drhops@gmail.com>
2013-02-25 01:50:19 -0800

    add test

--- test/cloverfs/core_test.clj
+++ test/cloverfs/core_test.clj
@@ -2,6 +2,19 @@
   (:use clojure.test
         cloverfs.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest mkdir
+  (let [root (create-directory),
+        answer {:children {:foo {:children {:bar {:children {:baz {:children {}}}}}}}}]
+	  (testing "mkdir"
+	    (is (= answer (execute-line root "mkdir foo/bar/baz"))))))
+
+(deftest touch-and-append
+  (let [answer-touch {:children {:the {:children {:sky {:contents ""}}}}},
+        answer-append {:children {:the {:children {:sky {:contents "blue blue cloud blue blue cloud cloud blue blue blue"}}}}},
+        root (create-directory),
+        result-touch (execute-line root "touch the/sky"),
+        result-append (execute-line result-touch "append the/sky blue blue cloud blue blue cloud cloud blue blue blue")]
+	  (testing "touch"
+	    (is (= answer-touch result-touch)))
+	  (testing "append"
+	    (is (= answer-append result-append)))))
